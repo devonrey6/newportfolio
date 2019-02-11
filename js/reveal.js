95cd481a943e570dc88f196b76dc8b775ae22e3c
unknown <devonreydonasco@gmail.com>
2019-02-12 03:05:57 +0800

    Updates

---
+++ js/reveal.js
@@ -0,0 +1,80 @@
+window.sr = ScrollReveal();
+sr.reveal("#navbar", {
+  duration: 2000,
+  origin: "left",
+  distance: "400px"
+});
+sr.reveal(".fa-gem", {
+  duration: 2000,
+  origin: "left",
+  distance: "400px"
+});
+sr.reveal(".code-hard", {
+  duration: 2000,
+  origin: "left",
+  distance: "400px"
+});
+sr.reveal(".story", {
+  duration: 2000,
+  origin: "top",
+  distance: "70px"
+});
+sr.reveal(".passion", {
+  duration: 2000,
+  origin: "left",
+  distance: "70px"
+});
+sr.reveal(".efficient", {
+  duration: 2000
+});
+sr.reveal(".hardwork", {
+  duration: 2000,
+  origin: "right",
+  distance: "70px"
+});
+sr.reveal(".portfolio", {
+  duration: 2000,
+  origin: "left",
+  distance: "70px"
+});
+sr.reveal(".image-portfolio", {
+  duration: 2000,
+  origin: "right",
+  distance: "70px"
+});
+sr.reveal(".about", {
+  duration: 2000,
+  origin: "top",
+  distance: "70px"
+});
+sr.reveal(".img-1", {
+  duration: 2000,
+  origin: "left",
+  distance: "70px"
+});
+sr.reveal(".img-2", {
+  duration: 2000
+});
+sr.reveal(".img-3", {
+  duration: 2000,
+  origin: "right",
+  distance: "70px"
+});
+sr.reveal(".reach", {
+  duration: 2000
+});
+sr.reveal(".social", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "70px"
+});
+sr.reveal(".table-contact", {
+  duration: 2000,
+  origin: "top",
+  distance: "50px"
+});
+sr.reveal(".copyright", {
+  duration: 2000,
+  origin: "left",
+  distance: "400px"
+});
